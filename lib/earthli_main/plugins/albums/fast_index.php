f9d14471ddd59e60a0d7f36aab4afab3a6eb407c
root <root@earthli.com>
2009-01-18 16:56:15 +0100

    Removed earthli-specific files

--- lib/earthli_main/plugins/albums/fast_index.php
+++
@@ -1,5 +0,0 @@
-<?php
-  $show_only_root_folders = TRUE;
-  $sort_by_modified = TRUE;
-  require_once ('webcore/pages/index.php');
-?>
--- lib/earthli_main/systems/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require_once ('webcore/init.php');
-
-  $Page->title->group = 'earthli Systems';
-  $Page->template_options->title = 'Systems';
-  $Page->template_options->icon = '{site_icons}products/systems';
-
-  $Page->location->add_root_link ();
-?>
