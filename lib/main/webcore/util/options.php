12b52043f24d81549b2864620298b75503c1c056
Marco von Ballmoos <marco@earthli.com>
2009-03-15 13:17:31 +0100

    Added missing chrome browser logos for GIF icons

--- lib/main/webcore/util/options.php
+++ lib/main/webcore/util/options.php
@@ -121,7 +121,7 @@ class STORED_OPTION extends WEBCORE_OBJECT
    */
   function _url_for_value ($value)
   {
-    $Result = new URL ('set_option.php');
+    $Result = new URL ('{' . Folder_name_pages . '}set_option.php');
     $Result->add_argument ('opt_name', $this->name);
     $Result->add_argument ('opt_value', $value);
     $Result->add_argument ('opt_page_context', $this->context->is_page);
