e17a914c9d946c199efcef4f9e1ccb3c653f470d
Marco Von Ballmoos <marco@earthli.com>
2021-01-09 18:09:31 +0100

    Fix lessc

--- scripts/less-all.sh
+++ scripts/less-all.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 FOLDER="../site/resources/styles/themes"
+LESS=../node_modules/less/bin/lessc
 
-for file in $FOLDER/*.less; do lessc $file $FOLDER/`basename $file .less`.css ; done
+MASK="$1*"
+
+for file in "$FOLDER"/$MASK.less; do
+  ECHO "Processing $file..."
+  $LESS "$file" $FOLDER/"$(basename "$file" .less)".css ;
+done
--- scripts/less-single.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-FOLDER="../site/resources/styles/themes"
-FILE=$1.less
-
-lessc $FOLDER/$FILE $FOLDER/`basename $FILE .less`.css
